061e43de349f7b97b323dc645f3d8dbc125c34e6
Sean Morrison <s3251350@student.rmit.edu.au>
2019-10-21 00:53:27 +1100

    config update

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 waypoints = 3
 hidden_dim = 256
 lr = 1e-4
-num_envs = 2
-batch_size = 16
-iterations = 10
-log_interval = 1
+num_envs = 32
+batch_size = 512
+iterations = 1000
+log_interval = 10
 runs = 1
